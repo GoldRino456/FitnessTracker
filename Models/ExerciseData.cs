2226406b7ba3e2f62e50e0443d65fd2772b51083
Ethan E. <eastwoode375@gmail.com>
2025-09-21 13:17:33 -0500

    Create Project, Add Basic Data Models

---
+++ Models/ExerciseData.cs
@@ -0,0 +1,13 @@
+﻿namespace FitnessTracker.Models;
+
+public class ExerciseData
+{
+    public string ExerciseId { get; set; }
+    public string Name { get; set; }
+    public string GifUrl { get; set; }
+    public List<string> TargetMuscles { get; set; }
+    public List<string> BodyParts { get; set; }
+    public List<string> Equipments { get; set; }
+    public List<string> SecondaryMuscles { get; set; }
+    public List<string> Instructions { get; set; }
+}
---
+++ Models/LogEntryData.cs
@@ -0,0 +1,13 @@
+﻿namespace FitnessTracker.Models;
+
+public class LogEntryData
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public string ExerciseName { get; set; }
+    public string ExerciseId { get; set; }
+    public int Sets { get; set; }
+    public int Reps { get; set; }
+    public float Time {  get; set; }
+    public bool IsTimeBasedExercise { get; set; }
+}
