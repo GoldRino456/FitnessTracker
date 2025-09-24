bd21c3e930d6859ba35b980b0effa209c7a3bc47
Ethan E. <eastwoode375@gmail.com>
2025-09-24 17:00:08 -0500

    Implemented Search

--- Pages/ExerciseSearch.cshtml.cs
+++ Pages/ExerciseSearch.cshtml.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FitnessTracker.Data.Models;
+using FitnessTracker.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel;
 
 namespace FitnessTracker.Pages
 {
     public class ExerciseSearchModel : PageModel
     {
         private readonly ILogger<ExerciseSearchModel> _logger;
+
+        [BindProperty]
+        public IList<ExerciseData> SearchResults { get; set; }
+
+        [BindProperty]
+        [DisplayName("Search")]
+        public string SearchQuery { get; set; } = String.Empty;
 
         public ExerciseSearchModel(ILogger<ExerciseSearchModel> logger)
         {
@@ -14,6 +24,31 @@ namespace FitnessTracker.Pages
 
         public void OnGet()
         {
+            SearchResults ??= [];
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await FetchSearchResultsAsync(SearchQuery);
+            return Page();
+        }
+
+        private async Task<bool> FetchSearchResultsAsync(string query)
+        {
+            var results = await ExerciseApiService.GetFuzzySearchResults(query);
+
+            if(results != null && results.Count > 0)
+            {
+                SearchResults = results;
+                return true;
+            }
+
+            return false;
         }
     }
 
